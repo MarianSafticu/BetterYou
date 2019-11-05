3823a2496fb5a2caa104f10758950390c44b6c61
Raluca Penciuc <penciuc.raluca@yahoo.com>
2019-11-06 00:55:50 +0200

    last style for tonight

--- frontend/better-you/src/components/RegisterBarComponent.tsx
+++ frontend/better-you/src/components/RegisterBarComponent.tsx
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import "../assets/scss/RegisterBarStyle.scss";
+import { Button } from "@material-ui/core";
 
 export default class RegisterBarComponent extends Component {
-    render() {
-        return (
-            <div className="register-bar-container">
-                tes210210
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="register-bar-container">
+        <div className="register-bar-components">
+          <div className="register-bar-header">
+            <h1>Start organise your life</h1>
+          </div>
+          <div className="register-bar-button">
+            <Button
+                size="large"
+                color="primary"
+            >Join now</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
