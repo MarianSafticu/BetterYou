8ecd4acd669ed8c010f67aa2336613faa46f751c
unknown <sbengheci.vlad@yahoo.com>
2019-11-08 07:22:40 -0800

    Forgot to remove something

--- frontend/better-you/src/App.tsx
+++ frontend/better-you/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/Routes";
 import "./assets/scss/AppStyle.scss";
 import { StylesProvider } from '@material-ui/core/styles';
-import LoginComponent from './components/LoginComponent';
 
 export default class App extends Component {
   render() {
--- frontend/better-you/src/routes/Routes.tsx
+++ frontend/better-you/src/routes/Routes.tsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import { StartPageComponent } from "../components/StartPageComponent";
-import LoginRegisterTabComponent  from "../components/LoginRegisterTabComponent";
 
 export default class Routes extends Component {
     render() {
         return (
             <Switch>
-                <Route exact path="/" component={LoginRegisterTabComponent}/>
+                <Route exact path="/" component={StartPageComponent}/>
             </Switch>
         );
     }
