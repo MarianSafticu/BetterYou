5e77313c94cd09ff938d368e0a5291d69a6b2fa6
Florin Pricop <pfir2383@scs.ubbcluj.ro>
2019-11-21 20:39:17 +0200

    Rezolvat requesturi2

--- backend/better-you/src/main/java/ServerUI/RestServerStart.java
+++ backend/better-you/src/main/java/ServerUI/RestServerStart.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@ComponentScan("ServerUI")
+
 @SpringBootApplication
 public class RestServerStart {
     public static void main(String[] args) {
