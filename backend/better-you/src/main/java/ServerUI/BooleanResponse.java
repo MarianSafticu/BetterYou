f76f209da88696bfedd60fe388fca89aa0045224
Florin Pricop <pfir2383@scs.ubbcluj.ro>
2019-11-28 13:52:13 +0200

    Fix response without Json

---
+++ backend/better-you/src/main/java/ServerUI/BooleanResponse.java
@@ -0,0 +1,20 @@
+package ServerUI;
+
+public class BooleanResponse {
+    private boolean aBoolean;
+
+    public BooleanResponse() {
+    }
+
+    public BooleanResponse(boolean aBoolean) {
+        this.aBoolean = aBoolean;
+    }
+
+    public boolean isaBoolean() {
+        return aBoolean;
+    }
+
+    public void setaBoolean(boolean aBoolean) {
+        this.aBoolean = aBoolean;
+    }
+}
