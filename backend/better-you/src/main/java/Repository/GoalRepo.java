1bfe172189d8248e7504854188f0c994b68ba8b5
Razvan <razvan_roatis@yahoo.com>
2019-11-29 21:16:40 +0200

    Implemented get user habits and goals

--- backend/better-you/src/main/java/Repository/GoalRepo.java
+++ backend/better-you/src/main/java/Repository/GoalRepo.java
@@ -3,9 +3,16 @@ package Repository;
 import Model.Goal;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
-public class GoalRepo extends AbstractRepo<Long, Goal>{
-    public GoalRepo(){
+public class GoalRepo extends AbstractRepo<Long, Goal> {
+    public GoalRepo() {
         super(Goal.class);
+    }
+
+    public List<Goal> getUsersGoals(final long userId) {
+        return new ArrayList<>();
     }
 }
