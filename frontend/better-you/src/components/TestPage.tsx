85d6cb40a33cd337fc2d382baefdd2bb44144d65
Andreea Rusu <rusu18andreea@gmail.com>
2019-12-03 15:05:36 +0200

    Menu profile picture

--- frontend/better-you/src/components/TestPage.tsx
+++ frontend/better-you/src/components/TestPage.tsx
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import "../assets/scss/StartPageStyle.scss";
-import GoalList from "./GoalList";
-import HabitList from "./HabitList";
-
+import MenuProfilePicture from "./MenuProfilePicture";
 interface TestPageComponentProps {}
 
 interface TestPageComponentState {}
+
 
 export default class TestPageComponent extends Component<
   TestPageComponentProps,
@@ -14,13 +12,9 @@ export default class TestPageComponent extends Component<
   render() {
     return (
       <div>
-        <div>
-          <GoalList />
-        </div>
-        <div>
-          <HabitList />
-        </div>
+        <MenuProfilePicture image={"https://scontent.fclj2-1.fna.fbcdn.net/v/t1.0-9/p960x960/65599347_2311883428903442_7016303985434820608_o.jpg?_nc_cat=104&_nc_ohc=9QKSRuuC0JIAQmAZfqU_L5Q2YSH2OvRoeW2h4wjL3neS-TF16WVPQQTOg&_nc_ht=scontent.fclj2-1.fna&oh=25407343b7baa23c5f5edf00b274d1fc&oe=5E42CD00"}/>
       </div>
+
     );
   }
 }
