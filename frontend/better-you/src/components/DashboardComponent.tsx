5f5f465b993995955b73dc8b2f46a730d13a123e
Rad Roxana-Marinela <radroxanamarinela@gmail.com>
2019-12-04 16:08:40 +0200

    Added scroll bar, switch and add button

--- frontend/better-you/src/components/DashboardComponent.tsx
+++ frontend/better-you/src/components/DashboardComponent.tsx
@@ -1,9 +1,54 @@
 import React, { Component } from "react";
+import GoalList from "./GoalList";
+import "../assets/scss/DashboardPageStyle.scss";
+import HabitList from "./HabitList";
+import NewsfeedList from "./NewsfeedList";
+import Grid from '@material-ui/core/Grid';
+import Switch from '@material-ui/core/Switch';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 export default class DashboardComponent extends Component {
+  state = {
+    checked: false
+  };
+
+  toggleChecked = () => {
+    this.setState({checked : !this.state.checked})
+  };
+
   render() {
     return (
-        <h1>Hello there</h1>
+        <div id="wrapper">
+            <div className="list">
+              <div id="switch_add_bar">
+                <div id="switch_label">
+                    <Grid component="label" container alignItems="center" spacing={1}>
+                      <Grid item>Goals</Grid>
+                        <Grid item>
+                          <Switch id="switch"
+                            checked={this.state.checked}
+                            onChange={this.toggleChecked}
+                            color = "primary"
+                            value="this.state.checked"/>
+                        </Grid>
+                      <Grid item>Habits</Grid>
+                    </Grid>
+                  </div>
+                
+                <AddCircleIcon id="icon_add" fontSize="large"/>
+              </div>
+                
+              { !this.state.checked && <GoalList/> }
+              {this.state.checked && <HabitList/> }
+            
+            </div>
+            
+            <div className="newsfeed">
+                <NewsfeedList/>
+            </div>
+
+        </div>
     );
   }
 }
+
--- frontend/better-you/src/components/dashboard-page/lists/newsfeed/NewsfeedList.tsx
+++ frontend/better-you/src/components/dashboard-page/lists/newsfeed/NewsfeedList.tsx
@@ -26,7 +26,63 @@ const newsfeedList = [
     name: "Maria",
     action:
       'added a new goal "Citeste 10 carti" ala bala portocala unu doi trei patru cinci sase sapte opt. Ala bala potrocala!'
-  }
+  },
+  {
+    image:
+      "https://www.feinberg.northwestern.edu/research/images/people/e-pugh-tall.jpg",
+    name: "Maria",
+    action:
+      'added a new goal "Citeste 10 carti" ala bala portocala unu doi trei patru cinci sase sapte opt. Ala bala potrocala!'
+  },
+  {
+    image:
+      "https://www.feinberg.northwestern.edu/research/images/people/e-pugh-tall.jpg",
+    name: "Maria",
+    action:
+      'added a new goal "Citeste 10 carti" ala bala portocala unu doi trei patru cinci sase sapte opt. Ala bala potrocala!'
+  },
+  {
+    image:
+      "https://www.feinberg.northwestern.edu/research/images/people/e-pugh-tall.jpg",
+    name: "Maria",
+    action:
+      'added a new goal "Citeste 10 carti" ala bala portocala unu doi trei patru cinci sase sapte opt. Ala bala potrocala!'
+  },
+  {
+    image:
+      "https://www.feinberg.northwestern.edu/research/images/people/e-pugh-tall.jpg",
+    name: "Maria",
+    action:
+      'added a new goal "Citeste 10 carti" ala bala portocala unu doi trei patru cinci sase sapte opt. Ala bala potrocala!'
+  },
+  {
+    image:
+      "https://www.feinberg.northwestern.edu/research/images/people/e-pugh-tall.jpg",
+    name: "Maria",
+    action:
+      'added a new goal "Citeste 10 carti" ala bala portocala unu doi trei patru cinci sase sapte opt. Ala bala potrocala!'
+  },
+  {
+    image:
+      "https://www.feinberg.northwestern.edu/research/images/people/e-pugh-tall.jpg",
+    name: "Maria",
+    action:
+      'added a new goal "Citeste 10 carti" ala bala portocala unu doi trei patru cinci sase sapte opt. Ala bala potrocala!'
+  },
+  {
+    image:
+      "https://www.feinberg.northwestern.edu/research/images/people/e-pugh-tall.jpg",
+    name: "Maria",
+    action:
+      'added a new goal "Citeste 10 carti" ala bala portocala unu doi trei patru cinci sase sapte opt. Ala bala potrocala!'
+  },
+  {
+    image:
+      "https://www.feinberg.northwestern.edu/research/images/people/e-pugh-tall.jpg",
+    name: "Maria",
+    action:
+      'added a new goal "Citeste 10 carti" ala bala portocala unu doi trei patru cinci sase sapte opt. Ala bala potrocala!'
+  },
 ];
 
 class NewsfeedList extends React.Component {
