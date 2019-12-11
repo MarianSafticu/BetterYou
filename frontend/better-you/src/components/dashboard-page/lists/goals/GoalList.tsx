5c7233ae9cfdd3c3b79b485a1577a816bf86700e
Rad Roxana-Marinela <radroxanamarinela@gmail.com>
2019-12-11 17:51:57 +0200

    Dashboard page fixed

--- frontend/better-you/src/components/dashboard-page/lists/goals/GoalList.tsx
+++ frontend/better-you/src/components/dashboard-page/lists/goals/GoalList.tsx
@@ -75,6 +75,116 @@ const goalsList: Goal[] = [
     endDate: new Date(),
     startDate: new Date(),
     category: "DICKS"
+  },
+  {
+    title: "Citeste 10 carti",
+    description: "aaa",
+    currentProgress: 10,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  {
+    title: "Citeste 11 carti",
+    description: "bbb",
+    currentProgress: 15,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  {
+    title: "Citeste 12 carti",
+    description: "ccc",
+    currentProgress: 66,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  
+  {
+    title: "Citeste 11 carti",
+    description: "bbb",
+    currentProgress: 15,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  {
+    title: "Citeste 12 carti",
+    description: "ccc",
+    currentProgress: 66,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  
+  {
+    title: "Citeste 11 carti",
+    description: "bbb",
+    currentProgress: 15,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  {
+    title: "Citeste 12 carti",
+    description: "ccc",
+    currentProgress: 66,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  {
+    title: "Citeste 13 carti",
+    description: "ddd",
+    currentProgress: 99,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  {
+    title: "Citeste 14 carti",
+    description: "eee",
+    currentProgress: 100,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  {
+    title: "Citeste 15 carti",
+    description: "fff",
+    currentProgress: 50,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  {
+    title: "Citeste 16 carti",
+    description: "ggg",
+    currentProgress: 24,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
+  },
+  {
+    title: "Citeste 17 carti",
+    description: "hhh",
+    currentProgress: 24,
+    progressToReach: 100,
+    endDate: new Date(),
+    startDate: new Date(),
+    category: "DICKS"
   }
 ];
 
