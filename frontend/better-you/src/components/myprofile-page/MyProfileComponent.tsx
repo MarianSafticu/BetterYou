bcba4b93def66920008caf78d3bbff292544ebee
Rad Roxana-Marinela <radroxanamarinela@gmail.com>
2020-01-16 17:55:07 +0200

    Created MyProfilePage and route

---
+++ frontend/better-you/src/components/myprofile-page/MyProfileComponent.tsx
@@ -0,0 +1,10 @@
+import React, { Component, RefObject } from "react";
+
+export class MyProfileComponent extends Component{
+    render() {
+        return (
+            <p>My profile page</p>
+
+        );
+    }
+}
