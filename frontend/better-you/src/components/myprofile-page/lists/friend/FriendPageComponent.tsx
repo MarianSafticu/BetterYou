77d204874e510e0348f4574305f407644613e8fb
PatiVioleta <48491987+PatiVioleta@users.noreply.github.com>
2020-01-17 22:26:15 +0200

    Aranjare butoane filter sort si add

--- frontend/better-you/src/components/myprofile-page/lists/friend/FriendPageComponent.tsx
+++ frontend/better-you/src/components/myprofile-page/lists/friend/FriendPageComponent.tsx
@@ -2,50 +2,57 @@ import React from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 import UserDTO from "../../../../models/UserDTO";
 import GoalList from "../../../dashboard-page/lists/goals/GoalList";
-import "../../../../assets/scss/dashboard-page/FriendPageStyle.scss"
+import "../../../../assets/scss/dashboard-page/FriendPageStyle.scss";
 import { Button } from "@material-ui/core";
 
 interface IProps {
-    username: string,
+  username: string;
 }
 
 interface IState {
-    user: UserDTO
+  user: UserDTO;
 }
 
+class FriendPageComponent extends React.Component<
+  RouteComponentProps<IProps>,
+  IState
+> {
+  getUser = (username: string): UserDTO => {
+    return {
+      username: username,
+      profilePicture:
+        "https://c8.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg",
+      isAuthenticated: true
+    };
+  };
 
-class FriendPageComponent extends React.Component<RouteComponentProps<IProps>, IState> {
-    getUser = (username: string): UserDTO => {
-        return { username: username, profilePicture: "https://c8.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg", isAuthenticated: true }
-    }
+  constructor(props: RouteComponentProps<IProps>) {
+    super(props);
+    var user = this.getUser(this.props.match.params.username);
+    this.state = {
+      user: user
+    };
+    console.log(this.props.match.params.username);
+  }
 
-    constructor(props: RouteComponentProps<IProps>) {
-        super(props);
-        var user = this.getUser(this.props.match.params.username)
-        this.state = {
-            user: user
-        }
-        console.log(this.props.match.params.username)
-    }
-
-    render() {
-        return (
-            <div className="profile-card-container">
-                <div className="profile-card-border">
-                    <div className="profile-card">
-                        <img src={this.state.user.profilePicture} />
-                        <div>
-                            <h2>User: {this.state.user.username} </h2>
-                            <Button> Add friend </Button>
-                            <Button> Challenge friend </Button>
-                        </div>
-                    </div>
-                </div>
-                <div className="goallist-container-div">
-                    <GoalList isReadOnly={true} />
-                </div>
+  render() {
+    return (
+      <div className="profile-card-container">
+        <div className="profile-card-border">
+          <div className="profile-card">
+            <img src={this.state.user.profilePicture} />
+            <div>
+              <h2>User: {this.state.user.username} </h2>
+              <Button> Add friend </Button>
+              <Button> Challenge friend </Button>
             </div>
-        )
-    }
+          </div>
+        </div>
+        <div className="goallist-container-div">
+          <GoalList isReadOnly={true} />
+        </div>
+      </div>
+    );
+  }
 }
 export default withRouter(FriendPageComponent);
