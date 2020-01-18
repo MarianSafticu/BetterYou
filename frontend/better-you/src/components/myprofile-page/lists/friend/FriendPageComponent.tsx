66c63740de8b341abb1d4ebd3acc6eeb41e06e21
PatiVioleta <48491987+PatiVioleta@users.noreply.github.com>
2020-01-18 12:27:41 +0200

    Fix

--- frontend/better-you/src/components/myprofile-page/lists/friend/FriendPageComponent.tsx
+++ frontend/better-you/src/components/myprofile-page/lists/friend/FriendPageComponent.tsx
@@ -4,23 +4,23 @@ import UserDTO from "../../../../models/UserDTO";
 import GoalList from "../../../dashboard-page/lists/goals/GoalList";
 import "../../../../assets/scss/dashboard-page/FriendPageStyle.scss";
 import { Button } from "@material-ui/core";
-import { acceptFriendBegin, addFriendBegin } from "../../../../redux/actions/actions";
+import {
+  acceptFriendBegin,
+  addFriendBegin
+} from "../../../../redux/actions/actions";
 import AppState from "../../../../redux/store/store";
 import { connect } from "react-redux";
 
-interface IProps extends RouteComponentProps<any>{
-    username: string,
-    addFriend: Function
+interface IProps extends RouteComponentProps<any> {
+  username: string;
+  addFriend: Function;
 }
 
 interface IState {
   user: UserDTO;
 }
 
-class FriendPageComponent extends React.Component<
-  RouteComponentProps<IProps>,
-  IState
-> {
+class FriendPageComponent extends React.Component<IProps, IState> {
   getUser = (username: string): UserDTO => {
     return {
       username: username,
@@ -30,42 +30,35 @@ class FriendPageComponent extends React.Component<
     };
   };
 
-class FriendPageComponent extends React.Component<IProps, IState> {
-    getUser = (username: string): UserDTO => {
-        return { username: username, profilePicture: "https://c8.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg", isAuthenticated: true }
-    }
+  constructor(props: IProps) {
+    super(props);
+    var user = this.getUser(this.props.match.params.username);
+    this.state = {
+      user: user
+    };
+    console.log(this.props.match.params.username);
+  }
 
-    constructor(props: IProps) {
-        super(props);
-        var user = this.getUser(this.props.match.params.username)
-        this.state = {
-            user: user
-        }
-        console.log(this.props.match.params.username)
-    }
+  makeFriendRequest = () => {
+    this.props.addFriend(this.state.user.username);
+  };
 
-    makeFriendRequest = () => {
-        this.props.addFriend(this.state.user.username);
-    }
-
-    render() {
-        return (
-            <div className="profile-card-container">
-                <div className="profile-card-border">
-                    <div className="profile-card">
-                        <img src={this.state.user.profilePicture} />
-                        <div>
-                            <h2>User: {this.state.user.username} </h2>
-                            <Button onClick={this.makeFriendRequest}> Add friend </Button>
-                        </div>
-                    </div>
-                </div>
-                <div className="goallist-container-div">
-                    <GoalList isReadOnly={true} />
-                </div>
+  render() {
+    return (
+      <div className="profile-card-container">
+        <div className="profile-card-border">
+          <div className="profile-card">
+            <img src={this.state.user.profilePicture} />
+            <div>
+              <h2>User: {this.state.user.username} </h2>
+              <Button onClick={this.makeFriendRequest}> Add friend </Button>
             </div>
           </div>
         </div>
+        <div className="goallist-container-div">
+          <GoalList isReadOnly={true} />
+        </div>
+
         <div className="goallist-container-div">
           <GoalList isReadOnly={true} />
         </div>
@@ -75,13 +68,15 @@ class FriendPageComponent extends React.Component<IProps, IState> {
 }
 
 const mapStateToProps = (state: AppState) => {
-    return {
-    }
-  }
-  
-  const mapDispatchToProps = (dispatch: any) => {
-    return {
-      addFriend: (username: string) => dispatch(addFriendBegin(username))
-    }
-  }
-export default  connect(mapStateToProps, mapDispatchToProps)(withRouter(FriendPageComponent));
+  return {};
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    addFriend: (username: string) => dispatch(addFriendBegin(username))
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(FriendPageComponent));
